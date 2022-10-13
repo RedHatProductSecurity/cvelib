869f64f95e200e6917444e37dca9e41e2fd05132
Martin Prpič <mprpic@redhat.com>
2022-10-13 09:48:52 -0400

    Check if the "name" object is missing in "user" object
    This is essentially a revert of f466ad44 with a link to a new issue where
    this was reported again since it doesn't seem the original issue was
    actually fixed.
    
    Resolves #40

--- cvelib/cli.py
+++ cvelib/cli.py
@@ -97,8 +97,12 @@ def print_user(user: dict) -> None:
 
 
 def get_full_name(user_data: dict) -> Optional[str]:
-    name_data = user_data["name"]
-    return f"{name_data.get('first', '')} {name_data.get('last', '')}".strip() or None
+    # If no name values are defined on a user, the entire `name` object is not returned in the
+    # user data response; see https://github.com/CVEProject/cve-services/issues/901.
+    name = user_data.get("name", {})
+    if name:
+        return f"{name.get('first', '')} {name.get('last', '')}".strip() or None
+    return None
 
 
 def bool_to_text(value: Optional[bool]) -> str:
