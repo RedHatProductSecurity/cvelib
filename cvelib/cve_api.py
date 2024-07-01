d681987ed73880eb5a0397e3f01cdb3f3106951a
Loïc Jaquemet <loic.jaquemet+github@gmail.com>
2024-07-01 12:25:28 -0600

    Load json schema with parse_float as decimal
    Float value in the json schema are used in arithmetic division. They must be loaded as decimal or comparison will fail.
    see #87

--- cvelib/cve_api.py
+++ cvelib/cve_api.py
@@ -51,7 +51,7 @@ class CveRecord:
             schema_path = cls.Schemas.CNA_PUBLISHED
 
         with open(schema_path) as schema_file:
-            schema = json.load(schema_file)
+            schema = json.load(schema_file, parse_float=decimal.Decimal)
 
         validator = Draft7Validator(schema)
         errors = sorted(validator.iter_errors(cve_json), key=lambda e: e.message)
